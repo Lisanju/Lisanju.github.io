e1fc8ef0e7edde17e2e692428f5f9b6b49a655fc
LIS-λ <106002045+Lisanju@users.noreply.github.com>
2024-02-20 16:57:57 -0300

    Update main.ff906fcfda52cf0be525.bundle.js

--- script/main.ff906fcfda52cf0be525.bundle.js
+++ script/main.ff906fcfda52cf0be525.bundle.js
@@ -631,7 +631,7 @@
             P = n(8);
         s.a.use(c.a);
         const M = [{
-            path: "/",
+            path: "/ocupação",
             name: "Ocupação",
             component: T
         }, {
